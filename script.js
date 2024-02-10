9c125083be58580cfbde4db35ec0912dc633e311
nuriddinj56 <remboxussi@gmail.com>
2024-02-10 10:29:04 +0500

    Js

---
+++ script.js
@@ -0,0 +1,73 @@
+'use strict'
+
+// Data
+// 1- account
+
+const account1 = {
+    password: "1111",
+    cardNumber: 86001402345678,
+    owner: {
+        fristName: "Nuriddin",
+        lastName: "Jo'rayev"
+    },
+    curreny: "USD",
+    locale: "en-EN",
+    tranfers: [
+        {
+            amount: 200,
+            datal: "2019-12-18T21:31:17.1782",
+        },
+        {
+            amount: 500,
+            datal: "2019-11-23T21:31:17.1782",
+        },
+        {
+            amount: 1100,
+            datal: "2019-12-18T09:31:17.1782",
+        },
+        {
+            amount: 1500,
+            datal: "2019-11-18T21:19:17.1782",
+        },
+        {
+            amount: -1100,
+            datal: "2019-12-18T06:31:17.3882",
+        }
+    ]
+}
+
+// 2-account
+
+const account2 = {
+    password: "1111",
+    cardNumber: 86001401965193,
+    owner: {
+        fristName: "Nuriddin",
+        lastName: "Jo'rayev"
+    },
+    curreny: "USD",
+    locale: "en-EN",
+    tranfers: [
+        {
+            amount: 5000,
+            datal: "2019-12-18T21:31:17.1782",
+        },
+        {
+            amount: -500,
+            datal: "2019-11-23T21:31:17.1782",
+        },
+        {
+            amount: 1300,
+            datal: "2019-12-18T09:31:17.1782",
+        },
+        {
+            amount: 2000,
+            datal: "2019-11-18T21:19:17.1782",
+        },
+        {
+            amount: -2200,
+            datal: "2019-12-18T06:31:17.3882",
+        }
+    ]
+}
+const accounts = [account1, account2]
