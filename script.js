2d28e0ba019f94159fe982aa8b2cd69878bf46e7
nuriddinj56 <remboxussi@gmail.com>
2024-02-11 08:40:37 +0500

    Card

--- script.js
+++ script.js
@@ -76,7 +76,12 @@ const accounts = [account1, account2]
 const inputLogin = document.querySelector('.login_input')
 const inputPassword = document.querySelector('.password_input')
 const btnLogin = document.querySelector('.enter')
-
+const brand = document.querySelector('h1')
+const formLogin = document.querySelector('form')
+const btnExit = document.querySelector('.exit')
+const cardOwner = document.querySelector('.user')
+const cardNumber = document.querySelector('.card_numbers')
+const cardBalans = document.querySelector('.balance')
 
 // Functions
 const createLogin = () => {
@@ -89,14 +94,51 @@ createLogin()
 
 // Login
 
+let loggedInAccount;   //boshshlanishiga undefined bo'lib turadi
 
+const currenyFormatte = (amount, locale, currency) => {
+    return amount.toLocaleString(locale, {
+        style: "currency",
+        currency,
+    })
+}
+const calcBalance = (tranfers) => {
+    return +tranfers.reduce((bal, tr) => bal + tr.amount, 0).toFixed(2)
+}
+const mackCreditCard = (cardNumber) => {
+    const str = cardNumber + ''
+    const last = str.slice(-4)
+    return last.padStart(str.length, '*')
+}
+const setCardInfo = (acc) => {
+    cardOwner.textContent = Object.values(loggedInAccount.owner).join(' ')
+    cardNumber.textContent = mackCreditCard(acc.cardNumber)
+    const bal = calcBalance(acc.tranfers)
+    cardBalans.textContent = currenyFormatte(bal, acc.locale, acc.curreny)
+}
+const setAllinfo = (acc) => {
+    setCardInfo(acc)
+}
 btnLogin.addEventListener('click', (e) => {
     e.preventDefault()
     const candidate = accounts.find(acc => acc.username === inputLogin.value)
-    if (!candidate) return console.log('login xato');
+    if (!candidate) return //console.log('login xato');
     if (candidate.password !== inputPassword.value) return
-    console.log('hush kelibsiz')
+    // console.log('hush kelibsiz')
+
+    loggedInAccount = candidate;
+    brand.textContent = `Hush kelibsiz ${candidate.owner.fristName} ${candidate.owner.lastName}`
+
+    inputLogin.value = ''
+    inputPassword.value = ''
+
+    formLogin.classList.add('hide')
+    btnExit.classList.remove('hide')
+    cardOwner.textContent = `${candidate.owner.fristName} ${candidate.owner.lastName}`
+
+    setAllinfo(loggedInAccount)
 })
+
 
 
 
